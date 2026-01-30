6563188ced8115c7987cdcb82696c1757f38e8d8
aRobert <124544863+RobertFilms@users.noreply.github.com>
2026-01-30 14:33:44 -0500

    Remove unnecessary console.log statements
    Cleaned up multiple controllers and socket handler files by removing debug and commented-out console.log statements. This improves code readability and reduces noise in server logs.

--- controllers/pixelControllers.js
+++ controllers/pixelControllers.js
@@ -273,7 +273,6 @@ async function setPixelsController(req, res) {
 
 		res.status(200).json({ message: 'ok' })
 	} catch (err) {
-		// console.log(err);
 		res.status(500).json({ error: 'There was a server error try again' })
 	}
 }
