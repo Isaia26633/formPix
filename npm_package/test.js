f56082964bbdbcfdd4dfc517cbefa8b2de0cad61
Isaiah Knaby <123019005+Isaia26633@users.noreply.github.com>
2026-04-01 12:54:39 -0400

    updated the NPM package to be up to date with the current API

--- npm_package/test.js
+++ npm_package/test.js
@@ -1,7 +1,16 @@
 const formpix = require('./formpixapi.js')
 
-formpix.login("http://172.16.3.101:421", "adecd2ce582e4f78038bde6ba1c618bf6737b14a3df57eb2ea5c7f3a51c08373ad6868c07960af47a0852ba629150cebcc7fa41ba1ce82ab5d719523aa01f5bd")
+formpix.login("http://172.16.3.101:421", "YOUR_API_KEY_HERE")
 
 // formpix.fill('#ff0000', 0, 10);
+// formpix.fillByPercent(50, '#ff0000', '#000000');
+// formpix.gradient('#ff0000', '#0000ff', 0, 10);
+// formpix.setPixel(0, '#ff0000');
+// formpix.setPixels([{ pixelNumber: 0, color: '#ff0000' }]);
+// formpix.progress({ fg1: '#00ff00', duration: 5000 });
+// formpix.rave({ mode: 'rainbow', speed: 50 });
+// formpix.raveStop();
 formpix.say('Hello World!', '#00ff00', '#0000ff');
-// formpix.playSound('19dfnc.wav', null)
+// formpix.getDisplay();
+// formpix.getSounds('formbar');
+// formpix.playSound('19dfnc.wav', null);
