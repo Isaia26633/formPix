3807009109cfa23f8a2f0a37dd12e4d2af1f6303
aRobert <124544863+RobertFilms@users.noreply.github.com>
2026-04-27 08:50:24 -0400

    Fixed counter disapearing

--- formPixSim/sockets/pollHandlers.js
+++ formPixSim/sockets/pollHandlers.js
@@ -256,12 +256,13 @@ function handleClassUpdate(webIo) {
 					if (shouldRedrawBoard) {
 						const boardStartPixel = config.barPixels
 						const boardLength = config.boards * 32 * 8
+						const counterLength = getStringColumnLength(text)
 						fill(pixels, 0x000000, boardStartPixel, boardLength)
 
-						let display = displayBoard(pixels, text, 0xFFFFFF, 0x000000, config, boardIntervals, ws281x)
+						let display = displayBoard(pixels, text, 0xFFFFFF, 0x000000, config, boardIntervals, ws281x, 0, counterLength)
 						if (display) boardIntervals.push(display)
 
-						display = displayBoard(pixels, pollText, 0xFFFFFF, 0x000000, config, boardIntervals, ws281x, getStringColumnLength(text))
+						display = displayBoard(pixels, pollText, 0xFFFFFF, 0x000000, config, boardIntervals, ws281x, counterLength)
 						if (display) boardIntervals.push(display)
 
 						state.lastPollBoardRenderKey = nextBoardRenderKey
